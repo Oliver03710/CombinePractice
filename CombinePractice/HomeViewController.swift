8dd06d4e1d9b20553e8443b0fbf7006f8d08e692
Oliver03710 <jhee2619@gmail.com>
2023-03-22 20:23:46 +0900

    [Feat] 기본 .sink {  }  사용

---
+++ CombinePractice/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  CombinePractice
+//
+//  Created by Junhee Yoon on 2023/03/22.
+//
+
+import UIKit
+import Combine
+
+final class HomeViewController: UIViewController {
+    // MARK: - Properties
+    private var subscription = Set<AnyCancellable>()
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let data = Datas(number: 20)
+        
+        data.$number
+            .sink { print("value = \($0)") }
+            .store(in: &subscription)
+        
+        data.number = 10
+        data.number = 1
+    }
+}
+
