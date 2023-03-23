9c70ada16cb918d9864a4f8b47a40ca8a7b59e5b
Oliver03710 <jhee2619@gmail.com>
2023-03-23 21:56:32 +0900

    [Feat] Scheduler - Receive(on:) 과 subscribe(on:)

--- CombinePractice/HomeViewController.swift
+++ CombinePractice/HomeViewController.swift
@@ -16,23 +16,46 @@ final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let subject = PassthroughSubject<String, Never>()
-        
+        let subject = PassthroughSubject<Void, Never>()
+
         subject
-            .sink(receiveValue: { print($0) })
+            .sink(receiveValue: { _ in print(Thread.isMainThread) })
             .store(in: &subscription)
+
+        subject.send(())
+
+        DispatchQueue.global().async {
+            subject.send(())
+        }
+
+        let cancellable = subject
+
+        cancellable
+          .handleEvents(receiveOutput: { print("upstream: \(Thread.isMainThread)") })
+          .receive(on: DispatchQueue.main)
+          .handleEvents(receiveOutput: { print("downstream: \(Thread.isMainThread)") })
+          .sink(receiveValue: { _ in print()  })
+          .store(in: &subscription)
+
+        DispatchQueue.global().async {
+          subject.send(())
+        }
         
-        subject.send("dddd")
-        subject.send("\(123123123)")
+        Just(1)
+          .map { _ in print("subscribe Upstream: \(Thread.isMainThread)") }
+          .map { _ in print("middle Stream: \(Thread.isMainThread)") }
+          .subscribe(on: DispatchQueue.global())
+          .sink { print("subscribe Downstream: \(Thread.isMainThread)") }
+          .store(in: &subscription)
         
-        let subject2 = CurrentValueSubject<String, Never>("Current")
+        let delayCancellable = Just(1)
         
-        subject2
-            .sink(receiveValue: { print($0) })
-            .store(in: &subscription)
-        
-        subject2.send("Next")
-        subject2.send("Next 2")
+        delayCancellable
+          .receive(on: DispatchQueue.main)
+          .map { _ in print(Thread.isMainThread) }
+          .delay(for: 2, scheduler: DispatchQueue.global())
+          .sink { print(Thread.isMainThread) }
+          .store(in: &subscription)
     }
 }
 
