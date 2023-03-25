312637f058fbfddb14f9242a66dbe1aa66fba087
Oliver03710 <jhee2619@gmail.com>
2023-03-25 15:21:36 +0900

    [feat] Operator: compactMap

--- CombinePractice/HomeViewController.swift
+++ CombinePractice/HomeViewController.swift
@@ -16,27 +16,10 @@ final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        [1, 2, -3, 4]
+        [1, 2, Optional(3), 4]
             .publisher
-            .tryFilter {
-                if $0 == -3 {
-                    throw CustomError.other
-                } else {
-                    return true
-                }
-            }
-            .sink(
-                receiveCompletion: { val in
-                    switch val {
-                    case let .failure(error):
-                        // 에러 발생시 Complition에서 처리하므로 그 뒤의 데이터는 구독해제 되어 출력되지 않음.
-                        print("error=", error)
-                    case .finished:
-                        print("finish")
-                    }
-                },
-                receiveValue: { print($0) }
-            )
+            .compactMap { $0 }
+            .sink(receiveValue: { print($0) })
             .store(in: &subscription)
     }
 }
