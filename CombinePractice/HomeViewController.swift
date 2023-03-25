02edc1f03e3bd8f9f647826e8398f8ed3e726709
Oliver03710 <jhee2619@gmail.com>
2023-03-25 14:24:07 +0900

    [Feat] Operator: Map

--- CombinePractice/HomeViewController.swift
+++ CombinePractice/HomeViewController.swift
@@ -16,46 +16,10 @@ final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let subject = PassthroughSubject<Void, Never>()
-
-        subject
-            .sink(receiveValue: { _ in print(Thread.isMainThread) })
-            .store(in: &subscription)
-
-        subject.send(())
-
-        DispatchQueue.global().async {
-            subject.send(())
-        }
-
-        let cancellable = subject
-
-        cancellable
-          .handleEvents(receiveOutput: { print("upstream: \(Thread.isMainThread)") })
-          .receive(on: DispatchQueue.main)
-          .handleEvents(receiveOutput: { print("downstream: \(Thread.isMainThread)") })
-          .sink(receiveValue: { _ in print()  })
-          .store(in: &subscription)
-
-        DispatchQueue.global().async {
-          subject.send(())
-        }
-        
-        Just(1)
-          .map { _ in print("subscribe Upstream: \(Thread.isMainThread)") }
-          .map { _ in print("middle Stream: \(Thread.isMainThread)") }
-          .subscribe(on: DispatchQueue.global())
-          .sink { print("subscribe Downstream: \(Thread.isMainThread)") }
-          .store(in: &subscription)
-        
-        let delayCancellable = Just(1)
-        
-        delayCancellable
-          .receive(on: DispatchQueue.main)
-          .map { _ in print(Thread.isMainThread) }
-          .delay(for: 2, scheduler: DispatchQueue.global())
-          .sink { print(Thread.isMainThread) }
-          .store(in: &subscription)
+        let publisher1 = [1, 2, 3].publisher
+        let cancellable1 = publisher1
+            .map { $0 + 2 }
+            .sink(receiveValue: { print($0) })
     }
 }
 
